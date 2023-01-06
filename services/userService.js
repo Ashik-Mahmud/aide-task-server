a974ace2288c0bd4791a7a2cdaa84b94ec793183
ashikmahmud <ashikmamud187@gmail.com>
2023-01-07 01:27:13 +0600

    create user service for user management

---
+++ services/userService.js
@@ -0,0 +1,29 @@
+const User = require("../models/userModel");
+
+/* Find user by email */
+exports.findUserByEmailService = async (email) => {
+  const user = await User.findOne({
+    email,
+  });
+  return user;
+};
+
+/* Find user by username */
+exports.findUserByUsernameService = async (username) => {
+  const user = await User.findOne({
+    username,
+  });
+  return user;
+};
+
+/* Find user by id */
+exports.findUserByIdService = async (id) => {
+  const user = await User.findById(id);
+  return user;
+};
+
+/* Create user */
+exports.createUserService = async (data) => {
+  const user = await User.create(data);
+  return user;
+};
